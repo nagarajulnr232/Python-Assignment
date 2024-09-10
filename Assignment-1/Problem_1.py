292fe977792851b70dc7b7f1298db91c0ad36e32
nagarajulnr232 <nagarajulnr232@gmail.com>
2024-09-10 14:15:59 +0530

    sample`

---
+++ Assignment-1/Problem_1.py
@@ -0,0 +1,67 @@
+# ## Print Your Name: Write a program that prints your name to the console.
+#
+# Name=input("Enter your name: ")
+# print(Name)
+#
+# ## Create a program that takes two numbers from the user and prints their sum.
+#
+# x=int(input("Enter num1: "))
+# y=int(input("Enter num2: "))
+# c=x+y
+# print("Sum ofsum1 & num2 :",c)
+#
+# ##using another method
+#
+# def sum(num1,num2):
+#     return num1+num2
+# result=sum(50,60)
+# print("Sum of num1 and num2:",result)
+#
+# ##using another method
+#
+# class sum:
+#     def __init__(self,x,y):
+#         self.num1 = x
+#         self.num2 = y
+#     def display(self):
+#         total =self.num1+self.num2
+#         print("Sum of two numbers num1 & num2:", total)
+# add_number=sum(49,50)
+# add_number.display()
+#
+# ##Write a program that calculates the area of a rectangle given its length and width.
+#
+# length=int(input("Enter length of the rectangle: "))
+# width=int(input("Enter width of the rectangle: "))
+# Area_rectangle=length*width
+# print(Area_rectangle)
+#
+# ## Using Another method
+#
+# def area_rectangle(length,width):
+#     return length*width
+# area=area_rectangle(40,60)
+# print("Area of the rectangle:",area)
+#
+# ## Using Another method
+#
+# class Rectangle:
+#     def __init__(self,length,width):
+#         self.length=length
+#         self.width=width
+#     def display(self):
+#         area_rectangle=self.length*self.width
+#         print("Length of the rectangle:",self.length)
+#         print("Width of the rectangle:",self.width)
+#         print("Area of the rectangle:",area_rectangle)
+# area=Rectangle(60,80)
+# area.display()
+#
+# ## Write a program that checks whether a number entered by the user is odd or even.
+#
+# num=int(input("Enter number: "))
+# if num%2== 0:
+#     print("Entered number is Even!")
+# else:
+#     print("Entered number is Odd")
+
