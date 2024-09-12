6dbf13535a74e5f57511547b3789f755bccb72bd
nagarajulnr232 <nagarajulnr232@gmail.com>
2024-09-12 11:06:22 +0530

    added_questions

--- Assignment-1/Problem_1.py
+++ Assignment-1/Problem_1.py
@@ -110,3 +110,42 @@
 # print(list1)
 
 # 8) Write a program that counts the number of vowels in a user-provided string.
+
+# string=input("Enter string:")
+# vowels=0
+# for i in string:
+#       if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+#             vowels=vowels+1
+# print("Number of vowels are:",vowels)
+
+# ## Another Method
+
+# string=input("Enter string: ")
+# vowels="aeiouAEIOU"
+# count=sum(string.count(vowel) for vowel in vowels )
+# print("The number of vowels in entered string is :",count)
+
+# ## Another method
+
+# string=input("Enter string : ")
+# vowels="aeiouAEIOU"
+# count =0
+# for a in vowels:
+#     counter = string.count(a)
+#     count = count + counter
+#
+# print(count)
+
+# 9) Create a program that takes a string as input and returns the reversed string.
+
+# def reverse(string):
+#     rev_string=string[::-1]
+#     return rev_string
+# string=input("Enter the string : ")
+# print("Reversed string :",reverse(string))
+
+# 10) Write a program that converts temperature from Celsius to Fahrenheit.
+
+# celsius = int(input("Enter the Temperature in Celsius : "))
+# fahrenheit = (1.8 * celsius) + 32
+# print("Temperature in Fahrenheit :", fahrenheit)
