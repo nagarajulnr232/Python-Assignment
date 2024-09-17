f33426558344f5afad73184c9f98c899128ecbef
nagarajulnr232 <nagarajulnr232@gmail.com>
2024-09-17 10:58:58 +0530

    Add_problem_7_sol

--- Assignment_week_2/problem_7_week_2.py
+++ Assignment_week_2/problem_7_week_2.py
@@ -1,2 +1,20 @@
 ## Write a program that takes a list of numbers and separates them into even and odd lists.
 
+def split_even_odd(value):
+    even=[]
+    odd=[]
+    for i in value:
+        if (i%2 == 0):
+            even.append(i)
+        else:
+            odd.append(i)
+    print("Main List:",value)
+    print("After separates even and odd lists")
+    print("Even list:",even)
+    print("Odd list:",odd)
+
+value=[12,14,13,11,45,38,71,62]
+split_even_odd(value)
+
+
+
